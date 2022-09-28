d52ebcdd23b698110286ad9aa8b0111f103902b6
kim-hyunjin <hyunjin1612@gmail.com>
2022-09-28 17:39:10 +0900

    more wagmi exam, update styles - no ssr - add css - balance, blocknumber exam

--- template/pages/example/wagmi.tsx
+++ template/pages/example/wagmi.tsx
@@ -1,106 +1,29 @@
-import * as React from 'react';
-import {
-  useAccount,
-  useConnect,
-  useDisconnect,
-  useEnsName,
-  useNetwork,
-  useSwitchNetwork,
-} from 'wagmi';
+import { Suspense } from 'react';
+import dynamic from 'next/dynamic';
+
+import styles from './wagmi.module.css';
+import BlockNumber from './BlockNumber';
+
+const Connect = dynamic(() => import('./Connect'), { ssr: false });
+const Account = dynamic(() => import('./Account'), { ssr: false });
+const NetworkSwitcher = dynamic(() => import('./NetworkSwitcher'), {
+  ssr: false,
+});
 
 function Page() {
-  const isMounted = useIsMounted();
-  const { isConnected } = useAccount();
-
   return (
-    <>
-      <Connect />
-
-      {isMounted && isConnected && (
-        <>
+    <div className={styles.container}>
+      <h1>Wagmi example</h1>
+      <Suspense fallback={<div>loading...</div>}>
+        <div>
+          <Connect />
           <Account />
           <NetworkSwitcher />
-        </>
-      )}
-    </>
+          <BlockNumber />
+        </div>
+      </Suspense>
+    </div>
   );
 }
 
 export default Page;
-
-function Connect() {
-  const isMounted = useIsMounted();
-  const { connector, isConnected } = useAccount();
-  const { connect, connectors, error, isLoading, pendingConnector } = useConnect();
-  const { disconnect } = useDisconnect();
-
-  return (
-    <div>
-      <div>
-        {isConnected && (
-          <button onClick={() => disconnect()}>Disconnect from {connector?.name}</button>
-        )}
-
-        {connectors
-          .filter((x) => isMounted && x.ready && x.id !== connector?.id)
-          .map((x) => (
-            <button key={x.id} onClick={() => connect({ connector: x })}>
-              {x.name}
-              {isLoading && x.id === pendingConnector?.id && ' (connecting)'}
-            </button>
-          ))}
-      </div>
-
-      {error && <div>{error.message}</div>}
-    </div>
-  );
-}
-
-function Account() {
-  const { address } = useAccount();
-  const { data: ensNameData } = useEnsName({ address });
-
-  return (
-    <div>
-      {ensNameData ?? address}
-      {ensNameData ? ` (${address})` : null}
-    </div>
-  );
-}
-
-function NetworkSwitcher() {
-  const { chain } = useNetwork();
-  const { chains, error, isLoading, pendingChainId, switchNetwork } = useSwitchNetwork();
-
-  return (
-    <div>
-      <div>
-        Connected to {chain?.name ?? chain?.id}
-        {chain?.unsupported && ' (unsupported)'}
-      </div>
-
-      {switchNetwork && (
-        <div>
-          {chains.map((x) =>
-            x.id === chain?.id ? null : (
-              <button key={x.id} onClick={() => switchNetwork(x.id)}>
-                {x.name}
-                {isLoading && x.id === pendingChainId && ' (switching)'}
-              </button>
-            )
-          )}
-        </div>
-      )}
-
-      <div>{error && error.message}</div>
-    </div>
-  );
-}
-
-function useIsMounted() {
-  const [mounted, setMounted] = React.useState(false);
-
-  React.useEffect(() => setMounted(true), []);
-
-  return mounted;
-}
